e845de2d7d1541da6daf45a3609d43f2e04ca794
Mr.T <147992689+NoPlayerCharacter@users.noreply.github.com>
2025-10-15 13:06:35 +0800

    refactor: improve error handling in getAllProducts function for better debugging

--- backend-aurashop/kasir-service/src/controllers/productCon.js
+++ backend-aurashop/kasir-service/src/controllers/productCon.js
@@ -1,8 +1,12 @@
 const Product = require("../models/productmodel");
 
+
 exports.getAllProducts = (req, res) => {
   Product.getAll((err, results) => {
-    if (err) return res.status(500).json({ message: "Gagal mengambil produk" });
+    if (err) {
+      console.error("Error:", err);
+      return res.status(500).json({ message: "Gagal mengambil data produk" });
+    }
     res.json(results);
   });
 };
