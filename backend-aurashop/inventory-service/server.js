0ff4aa49fee6a4dcfe65fd4b2496e83f021df50b
WilsonDaCosta02 <wilsondacosta0205@gmail.com>
2025-10-24 03:05:24 +0800

    server: pastikan mount `app.use('/api/inventory', productRoutes)` dan static /uploads

--- backend-aurashop/inventory-service/server.js
+++ backend-aurashop/inventory-service/server.js
@@ -3,12 +3,14 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const productRoutes = require('./routes/productRoutes');
 require('./config/db');
+const path = require('path');   
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/inventory', productRoutes);
 
 const PORT = 4001;
